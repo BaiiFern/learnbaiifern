2834654a2dfb803498883f7af2b2b74d24de0991
BaiiFern <gum-bie_fanclub@windowslive.com>
2016-04-24 14:29:27 +0700

    edit index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+var request = require('request');
+
+var express = require('express');
+var token = "CAACjWxNgVvoBANeQqSgormngorbs4OimsaNisCYi63jlbe8vZB5MkSd0830nuHI6Dvz8CfZAzLcZCMBIZA7ZAVfJbZBQa5uMJuTKRzi0N5KVRhK9WWdAOZATmJl24JrbmNgdNgkfZASdkvfE6ZAchNsa8YVfHZB5ijQ8EmuntNSDhTHiUhvkybKYIJ5VmcnfFzexh5KH6jTwLrZCwZDZD";
 
 app.set('port', (process.env.PORT || 5000));
 app.get('/', function (req, res) {
@@ -26,6 +32,27 @@ app.post('/webhook/', function (req, res) {
   }
   res.sendStatus(200);
 });
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');

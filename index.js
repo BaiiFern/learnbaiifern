ce37c7f6b485d705781883bab462313e34521398
BaiiFern <gum-bie_fanclub@windowslive.com>
2016-04-24 13:53:55 +0700

    edit index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
@@ -14,4 +15,8 @@ app.get('/webhook/', function (req, res) {
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });

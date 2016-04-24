ecaa04b14c9b2b121efd1b90cf8e38c19f7db38a
BaiiFern <gum-bie_fanclub@windowslive.com>
2016-04-24 13:40:47 +0700

    edit index.js

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/webhook/', function (req, res) {
+  if (req.query['hub.verify_token'] === 'baiifern') {
+    res.send(req.query['hub.challenge']);
+  }
+  res.send('Error, wrong validation token');
+})
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
